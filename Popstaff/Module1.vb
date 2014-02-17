3ada94dd1680eaa12ba911d1ad285e4c993b8589
kelvinct <kelvinct@yahoo.com.hk>
2014-02-17 11:45:10 +0800

    v2

---
+++ Popstaff/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+    Function IsValidEmailFormat(ByVal s As String) As Boolean
+        Try
+            Dim a As New System.Net.Mail.MailAddress(s)
+        Catch
+            Return False
+        End Try
+        Return True
+    End Function
+End Module
